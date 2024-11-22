ee5778f1f8266c19d30e53cd3753eb21f207836b
Bulus Hamnu <hamnubulus@gmail.com>
2024-11-22 12:53:18 +0100

    Added working files

---
+++ data/Music_links.js
@@ -0,0 +1,37 @@
+export const musicLink = [
+     {  name : "amazing",
+        image : "/",
+        appleMusic : `https://music.apple.com/us/album/amazing/1665203661?i=1665203663`,
+        spotify : `https://open.spotify.com/track/7HFPFNvm84vO7El1D8FluW?si=29fSyj1dQ9SLET88f2k9ng&context=spotify%3Aalbum%3A780drFYR8uKBOQqW5Urowo`,
+        youtube : `https://youtube.com/playlist?list=OLAK5uy_kDZK9vgG_vFhKbXIGaWF3InZb1m5jqtaM&si=ohjp30dma97Q1q95`,
+        soundCloud : `https://on.soundcloud.com/WeUWR`,
+        audioMack : `https://audiomack.com/demy-thekidd/song/amazing?share-user-id=26566107`
+    },
+    {   name : "specialLady" ,
+        image : "/",
+        appleMusic : `https://music.apple.com/us/album/special-lady/1737406018?i=1737406020`,
+        spotify : `https://open.spotify.com/track/61LsSjOUkLwlXAIaX0kDnB?si=H0F_jaquSz2csO-hL1No7A&context=spotify%3Aalbum%3A4rsixwrzTCHjQKfu85ShBn`,
+        youtube : `https://youtube.com/playlist?list=OLAK5uy_kvrLFmSdPIe-g053hpdBHz1BFdMBCc21k&si=vDVndY1WVt7kVdsf`,
+        soundCloud : `https://on.soundcloud.com/CFtCs`,
+        audioMack : `https://audiomack.com/demy-thekidd/song/special-lady?share-user-id=26566107`
+    },
+    {   name : "loml",
+        image : "/",
+        appleMusic : `https://music.apple.com/us/album/l-o-m-l/1690533760?i=1690533762`,
+        spotify : `https://open.spotify.com/album/1RmV8TqqnaOoe7wYGqXMxf?si=2wqAd4oHR6GaI06XoCRXZg`,
+        youtube : `https://youtube.com/playlist?list=OLAK5uy_lxj_Qu1MajKh4rh02lEqf4Y88MNMMjJnM&si=xjmOyXEZQ9eYIYR5`,
+        soundCloud : `https://on.soundcloud.com/ZkbKU`,
+        audioMack : `https://audiomack.com/demy-thekidd/song/loml?share-user-id=26566107`
+    },
+    {   name : "genesisEp",
+        image : "/",
+        appleMusic : `https://music.apple.com/us/album/genesis-ep/1769621984`,
+        spotify : `https://open.spotify.com/album/0bKKyaVBM6DDqSdkgIaWGh?si=Jml5E2gdTPWvpfIWEd4xHA`,
+        youtube : `https://youtube.com/playlist?list=OLAK5uy_l5cSksgxd5BO9TULgN4n9aRsqN6pYd6Tg&si=vebR3mg9106yGhBl`,
+        soundCloud : `https://on.soundcloud.com/iMR92`,
+        audioMack : `https://audiomack.com/demy-thekidd/album/genesis?share-user-id=26566107`
+    }
+]
+
+
+
