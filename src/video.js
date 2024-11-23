98b7f697c282276b3ae6d373579aaf006e11e54b
Bulus Hamnu <hamnubulus@gmail.com>
2024-11-23 21:56:40 +0100

    Work on the vedio.js to display all video from the artist yt channel through api

--- src/video.js
+++ src/video.js
@@ -1,17 +1,59 @@
-// vidio code
-
+// vidio codes
 
 let index = 0;
-let slideNum = document.querySelectorAll(".slide")
 let slideTime;
+let renderVedios;
+const slideContainer = document.querySelector(".slides")
+let getVideos = `https://www.googleapis.com/youtube/v3/search?part=snippet,id&channelId=UCx9k0JTrtI0jYwE_SLSX9LA&order=date&maxResults=50&key=AIzaSyC6XDzWuigY_TRwDwc78XPrYQmpafZm31U`;
+let slideNum;
+const slider = document.querySelector(".slider")
 
-document.addEventListener("DOMContentLoaded", () => {
+
+
+
+
+
+async function getVideo () {
+    let videoData;
+    let response = await fetch(getVideos);
+    let data = await response.json() 
+    let render = "";
+
+
+    videoData = data.items
+
+    videoData.forEach(song => {
+        render += `
+        <div class="slide">
+            <iframe  width="570" height="325" src="https://www.youtube.com/embed/${song.id.videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>       
+        </div>
+        `
+        
+        
+    })
+    slideContainer.innerHTML += render;
+    initiate ()
+}
+
+
+// getVideo()
+initiate ()
+
+
+function initiate () {
+    slideNum = document.querySelectorAll(".slide");
+    if (slideNum.length === 0) {
+        slider.innerHTML = "Sorry there is no video available!"
+        slider.style.marginTop = "3rem";
+        return;
+    }
+
     slideNum[index].classList.add("showSlide");
     slideTime = setInterval(() => {
         index++;
         nextSlide(index);
     },5000);
-})
+}
 
 function nextSlide (slideIndex) {
     if (slideIndex >= slideNum.length) {
@@ -35,16 +77,26 @@ function prevSlide(slideIndex) {
     }
 }
 
-document.querySelector(".forward").addEventListener("click",() => {
-    clearInterval(slideTime);
-    index++;
-    nextSlide(index);
-});
+const forwardButton = document.querySelector(".forward")
+const backwardButton = document.querySelector(".backward")
 
-document.querySelector(".backward").addEventListener("click",() => {
-    clearInterval(slideTime);
-    index--;
-    prevSlide(index);
-});
+if (forwardButton && backwardButton) {
+    forwardButton.addEventListener("click",() => {
+        clearInterval(slideTime);
+        index++;
+        nextSlide(index);
+    });
+    
+    backwardButton.addEventListener("click",() => {
+        clearInterval(slideTime);
+        index--;
+        prevSlide(index);
+    });
+}
+
+
+
+
+
 
 
