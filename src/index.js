ebd18233d78f5cc78d2a51854d49da864e4e53ea
Bulus Hamnu <hamnubulus@gmail.com>
2024-12-21 17:16:53 +0100

    add wrapper to contain the content on large screen, make the wrapper flex to position the footer at the bootom, change the style of the form,arrow and also edit some media query because it was causing a problem

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const closePopup = document.querySelector(".close-popup");
 
 
 
+
 contactMeBtn.addEventListener("click", function() { 
     contactForm.classList.add("show-popup-entry")
     backdrop.classList.toggle("active");
@@ -14,5 +15,29 @@ closePopup.addEventListener("click", function() {
     backdrop.classList.toggle("active");
  });
 
+
+ contactForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: "POST",
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    })
+        .then( response => {
+            if (response.ok) {
+                alert("Form submitted successfully! I'll get back to you shortly.");
+                contactForm.reset();
+
+                contactForm.classList.remove("show-popup-entry")
+                backdrop.classList.toggle("active");
+            }
+        
+        }).catch(err => { alert("there was an error submitting the form! please try again")})
+
+
+ })
 
 
