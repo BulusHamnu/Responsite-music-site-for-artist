41df19d62711fde0e67268d478033aa02f87fc78
Bulus Hamnu <hamnubulus@gmail.com>
2024-12-22 07:23:11 +0100

    remodify the files page again and reduces all images size

--- data/Music_links.js
+++ data/Music_links.js
@@ -1,6 +1,6 @@
 export const musicLink = [
      {  name : "amazing",
-        image : `../images/CoverArt/amazingArt.png`,
+        image : `images/CoverArt/amazingArt.png`,
         appleMusic : `https://music.apple.com/us/album/amazing/1665203661?i=1665203663`,
         spotify : `https://open.spotify.com/track/7HFPFNvm84vO7El1D8FluW?si=29fSyj1dQ9SLET88f2k9ng&context=spotify%3Aalbum%3A780drFYR8uKBOQqW5Urowo`,
         youtube : `https://youtube.com/playlist?list=OLAK5uy_kDZK9vgG_vFhKbXIGaWF3InZb1m5jqtaM&si=ohjp30dma97Q1q95`,
@@ -8,7 +8,7 @@ export const musicLink = [
         audioMack : `https://audiomack.com/demy-thekidd/song/amazing?share-user-id=26566107`
     },
     {   name : "specialLady" ,
-        image : "../images/CoverArt/specialLadyArt.jpeg",
+        image : "images/CoverArt/specialLadyArt.jpeg",
         appleMusic : `https://music.apple.com/us/album/special-lady/1737406018?i=1737406020`,
         spotify : `https://open.spotify.com/track/61LsSjOUkLwlXAIaX0kDnB?si=H0F_jaquSz2csO-hL1No7A&context=spotify%3Aalbum%3A4rsixwrzTCHjQKfu85ShBn`,
         youtube : `https://youtube.com/playlist?list=OLAK5uy_kvrLFmSdPIe-g053hpdBHz1BFdMBCc21k&si=vDVndY1WVt7kVdsf`,
@@ -16,7 +16,7 @@ export const musicLink = [
         audioMack : `https://audiomack.com/demy-thekidd/song/special-lady?share-user-id=26566107`
     },
     {   name : "loml",
-        image : "../images/CoverArt/lomlArt.png",
+        image : "images/CoverArt/lomlArt.png",
         appleMusic : `https://music.apple.com/us/album/l-o-m-l/1690533760?i=1690533762`,
         spotify : `https://open.spotify.com/album/1RmV8TqqnaOoe7wYGqXMxf?si=2wqAd4oHR6GaI06XoCRXZg`,
         youtube : `https://youtube.com/playlist?list=OLAK5uy_lxj_Qu1MajKh4rh02lEqf4Y88MNMMjJnM&si=xjmOyXEZQ9eYIYR5`,
@@ -24,7 +24,7 @@ export const musicLink = [
         audioMack : `https://audiomack.com/demy-thekidd/song/loml?share-user-id=26566107`
     },
     {   name : "genesis Ep",
-        image : `../images/CoverArt/genisisEpArt (1).jpg`,
+        image : `images/CoverArt/genisisEpArt (1).jpg`,
         appleMusic : `https://music.apple.com/us/album/genesis-ep/1769621984`,
         spotify : `https://open.spotify.com/album/0bKKyaVBM6DDqSdkgIaWGh?si=Jml5E2gdTPWvpfIWEd4xHA`,
         youtube : `https://youtube.com/playlist?list=OLAK5uy_l5cSksgxd5BO9TULgN4n9aRsqN6pYd6Tg&si=vebR3mg9106yGhBl`,
